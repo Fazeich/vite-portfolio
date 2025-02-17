155b7da4d0841d0fcb30cb8ae69d86d1d375dd3a
travnya <travn1k@inbox.ru>
2025-02-17 17:16:53 +0300

    proxy

--- client/configs/vite.config.ts
+++ client/configs/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3010",
+        target: "http://81.177.223.127:3010",
       },
     },
   },
